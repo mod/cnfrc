b6270184d138d01d33daf6e7f6e136424926ae98
MoD <mod@websys.io>
2014-10-12 04:39:50 +0200

    Adding dotfiles
    new file:   Xresources
    new file:   gitconfig
    new file:   vimrc
    new file:   xprofile
    new file:   zshrc

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -41,7 +41,7 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
@@ -55,17 +55,11 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts =
 {
-    awful.layout.suit.floating,
     awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
     awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
     awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
+    awful.layout.suit.floating,
     awful.layout.suit.magnifier
 }
 -- }}}
@@ -83,7 +77,8 @@ end
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+    tags[s] = awful.tag({ "1-[WEB]", "2-[WORK]", "3-[TERM]", "4-[LOG]",
+    "5-[SSH]", "6-[APP]", "7-[MISC]", "8-[FLOAT]", "9-[ROOT]" }, s, layouts[1])
 end
 -- }}}
 
---
+++ vimrc
@@ -0,0 +1,20 @@
+set smartindent
+set tabstop=2
+set shiftwidth=2
+set expandtab
+
+set splitright
+set splitbelow
+
+set autoindent
+set number
+set formatoptions+=ro
+set textwidth=76
+set modeline
+set scrolljump=2
+set scrolloff=99
+
+set nobackup
+set noswapfile
+
+set wildmenu
